e26f13e0075804236096ebe80ed3579604eebe45
fabiooo4 <fabioirimie@gmail.com>
2025-06-19 22:04:21 +0200

    fix: Updated js types

--- frontend/src/lib/types.ts
+++ frontend/src/lib/types.ts
@@ -1,6 +1,19 @@
 /* tslint:disable */
 /* eslint-disable */
-// Generated using typescript-generator version 3.2.1263 on 2025-06-02 01:01:49.
+// Generated using typescript-generator version 3.2.1263 on 2025-06-19 22:02:04.
 
-export interface DiabetesLoggerApplication {
+export interface Patient {
+    id: number;
+    firstName: string;
+    lastName: string;
+    age: number;
+    referralMedic: Medic;
+}
+
+export interface Medic {
+    id: number;
+    name: string;
+}
+
+export interface Therapy {
 }
