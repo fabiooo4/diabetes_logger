44f9ed4fcd4e6fef68018dc207814d3b5e80aca1
Magi <paoloimbrmagi@gmail.com>
2025-07-10 22:27:55 +0200

    merge

--- frontend/src/lib/types.ts
+++ frontend/src/lib/types.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
-// Generated using typescript-generator version 3.2.1263 on 2025-07-10 16:22:42.
+// Generated using typescript-generator version 3.2.1263 on 2025-07-10 16:41:35.
 
 export interface User {
     notifications: Notification[];
