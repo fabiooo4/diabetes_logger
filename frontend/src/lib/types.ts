b57758ea9de9c38e1daef8c79485d088a6d3f504
fabiooo4 <fabioirimie@gmail.com>
2025-07-13 21:56:07 +0200

    feat: Added favicon

--- frontend/src/lib/types.ts
+++ frontend/src/lib/types.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
-// Generated using typescript-generator version 3.2.1263 on 2025-07-10 22:17:24.
+// Generated using typescript-generator version 3.2.1263 on 2025-07-13 21:54:32.
 
 export interface User {
     notifications: Notification[];
